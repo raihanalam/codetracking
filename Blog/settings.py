a660bf368f0e02ed0a61b25acf01764a1f85c267
raihanalam <raihanhridoybd@gmail.com>
2022-01-02 04:10:45 +0600

    changed debug

--- Blog/settings.py
+++ Blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=c%!k@mx41x6i_=04=kx8m0sk8)bu8c2ud!3s@o5%7^l(!#j7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['codetracking.herokuapp.com', '127.0.0.1']
 
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
