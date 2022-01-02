3782eb6ac7eeb8087b8c6672fecb3acd5c5289e7
raihanalam <raihanhridoybd@gmail.com>
2022-01-03 00:36:58 +0600

    img change

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,12 +4,14 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('App_Login.urls')),
     path('blog/',include('App_Blog.urls')),
-    path('',views.Index,name='index')
+    path('',views.Index,name='index'),
+    
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
